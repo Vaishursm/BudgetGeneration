8cd6a1e694935c2d102580a5b0d4c2ca24c11f6f
Vaishali Sornamani <vsornamani@altimetrik.com>
2025-08-21 18:08:53 +0530

    Add next-themes dependency

--- global.d.ts
+++ global.d.ts
@@ -1,1 +1,24 @@
-declare module "next-themes";
+// global.d.ts
+declare module "next-themes" {
+  import * as React from "react";
+
+  export interface ThemeProviderProps {
+    attribute?: string;
+    defaultTheme?: string;
+    enableSystem?: boolean;
+    disableTransitionOnChange?: boolean;
+    themes?: string[];
+    forcedTheme?: string;
+    enableColorScheme?: boolean;
+    storageKey?: string;
+    value?: Record<string, string>;
+    children?: React.ReactNode;
+  }
+
+  export const ThemeProvider: React.FC<ThemeProviderProps>;
+  export const useTheme: () => {
+    theme: string | undefined;
+    setTheme: (theme: string) => void;
+    systemTheme: string | undefined;
+  };
+}
